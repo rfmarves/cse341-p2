188d7f0b0c8fe573877ebd32c19791032b4ed0b4
rfmarves <rfmarves@gmail.com>
2025-07-19 21:38:06 -0600

    added "try" error catching

--- controllers/events.js
+++ controllers/events.js
@@ -3,7 +3,8 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = (req, res) => {
 //#swagger.tags=['Events']
-  mongodb
+  try {
+    mongodb
     .getDb()
     .db()
     .collection('events')
@@ -15,84 +16,102 @@ const getAll = (req, res) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists);
     });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while fetching events.' });
+  }
 };
 
 const getSingle = (req, res) => {
 //#swagger.tags=['Events']
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Invalid ID.');
+  try{
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Invalid ID.');
+    }
+    const userId = new ObjectId(req.params.id);
+    mongodb
+      .getDb()
+      .db()
+      .collection('events')
+      .find({ _id: userId})
+      .toArray((err, lists) => {
+        if (err) {
+          res.status(400).json({ message: err});
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+      });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while fetching the event.' });
   }
-  const userId = new ObjectId(req.params.id);
-  mongodb
-    .getDb()
-    .db()
-    .collection('events')
-    .find({ _id: userId})
-    .toArray((err, lists) => {
-      if (err) {
-        res.status(400).json({ message: err});
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
 };
 
 const createEvent = async (req, res) => {
 //#swagger.tags=['Events']
-  const event = {
-    eventName: req.body.eventName,
-    eventDate: req.body.eventDate,
-    startTime: req.body.startTime,
-    capacity: req.body.capacity,
-    location: req.body.location
-  };
-  const response = await mongodb.getDb().db().collection('events').insertOne(event);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the event.');
+  try {
+    const event = {
+      eventName: req.body.eventName,
+      eventDate: req.body.eventDate,
+      startTime: req.body.startTime,
+      capacity: req.body.capacity,
+      location: req.body.location
+    };
+    const response = await mongodb.getDb().db().collection('events').insertOne(event);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while creating the event.');
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while creating the event.' });
   }
 };
 
 const updateEvent = async (req, res) => {
 //#swagger.tags=['Events']
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Invalid ID.');
-  }
-  const userId = new ObjectId(req.params.id);
-  // be aware of updateOne if you only want to update specific fields
-  const event = {
-    eventName: req.body.eventName,
-    eventDate: req.body.eventDate,
-    startTime: req.body.startTime,
-    capacity: req.body.capacity,
-    location: req.body.location
-  };
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('events')
-    .replaceOne({ _id: userId }, event);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the event.');
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Invalid ID.');
+    }
+    const userId = new ObjectId(req.params.id);
+    const event = {
+      eventName: req.body.eventName,
+      eventDate: req.body.eventDate,
+      startTime: req.body.startTime,
+      capacity: req.body.capacity,
+      location: req.body.location
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('events')
+      .replaceOne({ _id: userId }, event);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the event.');
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while updating the event.' });
   }
 };
 
 const deleteEvent = async (req, res) => {
 //#swagger.tags=['Events']
+  try {
     if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Invalid ID.');
-  }
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('events').remove({ _id: userId }, true);
-  console.log(response);
-  if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the event.');
+      res.status(400).json('Invalid ID.');
+    }
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('events').remove({ _id: userId }, true);
+    console.log(response);
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while deleting the event.');
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while deleting the event.' });
   }
 };
 
--- controllers/tickets.js
+++ controllers/tickets.js
@@ -13,113 +13,133 @@ function isInvalidObjectId(id){
 
 const getAll = (req, res) => {
 //#swagger.tags=['Tickets']
-  mongodb
-    .getDb()
-    .db()
-    .collection('tickets')
-    .find()
-    .toArray((err, lists) => {
-      if (err) {
-        res.status(400).json({ message: err});
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
+  try {
+    mongodb
+      .getDb()
+      .db()
+      .collection('tickets')
+      .find()
+      .toArray((err, lists) => {
+        if (err) {
+          res.status(400).json({ message: err});
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+      });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while fetching tickets.' });
+  }
 };
 
 const getSingle = (req, res) => {
 //#swagger.tags=['Tickets']
-  if (isInvalidObjectId(req.params.id)) {
-    res.status(400).json('Invalid ID.');
+  try {
+    if (isInvalidObjectId(req.params.id)) {
+      res.status(400).json('Invalid ID.');
+    }
+    const userId = new ObjectId(req.params.id);
+    mongodb
+      .getDb()
+      .db()
+      .collection('tickets')
+      .find({ _id: userId})
+      .toArray((err, lists) => {
+        if (err) {
+          res.status(400).json({ message: err});
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+      });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while fetching the ticket.' });
   }
-  const userId = new ObjectId(req.params.id);
-  mongodb
-    .getDb()
-    .db()
-    .collection('tickets')
-    .find({ _id: userId})
-    .toArray((err, lists) => {
-      if (err) {
-        res.status(400).json({ message: err});
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
 };
 
 const createTicket = async (req, res) => {
 //#swagger.tags=['Tickets']
-  if (isInvalidObjectId(req.body.eventId)) {
-    res.status(400).json('Invalid Event ID.');
-  } else {
-    const ticket = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    eventId: req.body.eventId,
-    amountPaid: req.body.amountPaid,
-    purchaseDate: req.body.purchaseDate,
-    paymentMethod: req.body.paymentMethod,
-    phoneNumber: req.body.phoneNumber,
-    address: req.body.address,
-    status: "registered",
-    };
-    const response = await mongodb.getDb().db().collection('tickets').insertOne(ticket);
-    if (response.acknowledged) {
-      res.status(201).json(response);
+  try {
+    if (isInvalidObjectId(req.body.eventId)) {
+      res.status(400).json('Invalid Event ID.');
     } else {
-      res.status(500).json(response.error || 'Some error occurred while creating the ticket.');
+      const ticket = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      eventId: req.body.eventId,
+      amountPaid: req.body.amountPaid,
+      purchaseDate: req.body.purchaseDate,
+      paymentMethod: req.body.paymentMethod,
+      phoneNumber: req.body.phoneNumber,
+      address: req.body.address,
+      status: "registered",
+      };
+      const response = await mongodb.getDb().db().collection('tickets').insertOne(ticket);
+      if (response.acknowledged) {
+        res.status(201).json(response);
+      } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the ticket.');
+      }
     }
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while creating the ticket.' });
   }
 };
 
 const updateTicket = async (req, res) => {
 //#swagger.tags=['Tickets']
-  if (isInvalidObjectId(req.body.eventId)) {
-    res.status(400).json('Invalid ID.');
-  }
-  if (isInvalidObjectId(req.body.eventId)) {
-    res.status(400).json('Invalid Event ID.');
-  } else {
-    const userId = new ObjectId(req.params.id);
-    const ticket = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    eventId: req.body.eventId,
-    amountPaid: req.body.amountPaid,
-    purchaseDate: req.body.purchaseDate,
-    paymentMethod: req.body.paymentMethod,
-    phoneNumber: req.body.phoneNumber,
-    address: req.body.address,
-    status: "modified",
-    };
-    const response = await mongodb
-      .getDb()
-      .db()
-      .collection('tickets')
-      .replaceOne({ _id: userId }, ticket);
-    console.log(response);
-    if (response.modifiedCount > 0) {
-      res.status(204).send();
-    } else {
-      res.status(500).json(response.error || 'Some error occurred while updating the ticket.');
+  try {
+    if (isInvalidObjectId(req.body.eventId)) {
+      res.status(400).json('Invalid ID.');
     }
+    if (isInvalidObjectId(req.body.eventId)) {
+      res.status(400).json('Invalid Event ID.');
+    } else {
+      const userId = new ObjectId(req.params.id);
+      const ticket = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      eventId: req.body.eventId,
+      amountPaid: req.body.amountPaid,
+      purchaseDate: req.body.purchaseDate,
+      paymentMethod: req.body.paymentMethod,
+      phoneNumber: req.body.phoneNumber,
+      address: req.body.address,
+      status: "modified",
+      };
+      const response = await mongodb
+        .getDb()
+        .db()
+        .collection('tickets')
+        .replaceOne({ _id: userId }, ticket);
+      console.log(response);
+      if (response.modifiedCount > 0) {
+        res.status(204).send();
+      } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the ticket.');
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while updating the ticket.' });
   }
 };
 
 const deleteTicket = async (req, res) => {
 //#swagger.tags=['Tickets']
+  try {
     if (isInvalidObjectId(req.params.id)) {
-    res.status(400).json('Invalid ID.');
-  }
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('tickets').remove({ _id: userId }, true);
-  console.log(response);
-  if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the ticket.');
+      res.status(400).json('Invalid ID.');
+    }
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('tickets').remove({ _id: userId }, true);
+    console.log(response);
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while deleting the ticket.');
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while deleting the ticket.' });
   }
 };
 
